8ef13ceb91cfffdeca27e2ba59d8a080e3264f35
CHIRAG LAL <chiraglal15@gmail.com>
2024-04-06 18:26:37 +0530

    minor changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ const Sidebar = () => {
     return (
         <div className='bg-slate-800 flex-none h-screen w-14 sm:w-20'>
             <div className='h-20 items-centre flex'>
-                <HomeIcon width={40} className='text-white left-3 sm:left-6 fixed' />
+                <HomeIcon width={40} className='text-gray-300 bg-gray-600 rounded-lg p-2 left-3 sm:left-6 fixed' />
             </div>
             <div className='left-3 sm:left-6 fixed top-[100px]'>
                 <ChartBarIcon width={40} className='bg-gray-600 p-2 rounded-lg mb-4 text-gray-300' />
